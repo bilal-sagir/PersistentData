66955a68fcc2e78760a98ed79ce7f9055b8c36c4
bilal-sagir <sagir.bilal@icloud.com>
2021-11-23 19:20:36 +0300

    read and write to disk

--- PersistentData/ViewController.swift
+++ PersistentData/ViewController.swift
@@ -2,22 +2,50 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    let myText = """
+                    a long text
+                    with multiple
+                    different long
+                    lines.
+                    """
+    
+    func write ()
+    {
+        let fileManager = FileManager.default
+        
+        do{
+            let myDocs = try fileManager.url(for: .documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
+            let file: URL = myDocs.appendingPathComponent("file.txt")
+            print(file)
+            try? myText.write(to: file, atomically: true, encoding: String.Encoding.utf8)
+        }catch{print(error)}
+        
+    }
+    
+    func read ()
+    {
+        let fileManager = FileManager.default
+        
+        do{
+            let myDocs = try fileManager.url(for: .documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
+            let file: URL = myDocs.appendingPathComponent("file.txt")
+            
+            let content = try String(contentsOf: file)
+            
+            print(content)
+        }catch{print(error)}
+        
+    }
 
+    
+
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        //write()
+        read()
         
-        let defaults = UserDefaults.standard
-        defaults.set("true", forKey: "firstUsed")
-        defaults.synchronize()
-        
-        // < 100kb
-        
-        if let useStatus = defaults.object(forKey: "firstUsed")
-        {
-            print("status is : \(useStatus)")
-            
-        }
     }
 
 }
