1af43b1bcaaeb185ffd8f14d59c903b2437a29ac
bilal-sagir <sagir.bilal@icloud.com>
2021-11-23 18:03:17 +0300

    initial

--- PersistentData/ViewController.swift
+++ PersistentData/ViewController.swift
@@ -1,19 +1,12 @@
-//
-//  ViewController.swift
-//  PersistentData
-//
-//  Created by Bilal on 23.11.2021.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
