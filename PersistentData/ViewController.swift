a843e603d8f80ebc2078bd1cc7cd9e521d62f023
bilal-sagir <sagir.bilal@icloud.com>
2021-11-23 18:07:36 +0300

    defaultuser

--- PersistentData/ViewController.swift
+++ PersistentData/ViewController.swift
@@ -6,6 +6,18 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        defaults.set("true", forKey: "firstUsed")
+        defaults.synchronize()
+        
+        // < 100kb
+        
+        if let useStatus = defaults.object(forKey: "firstUsed")
+        {
+            print("status is : \(useStatus)")
+            
+        }
     }
 
 }
