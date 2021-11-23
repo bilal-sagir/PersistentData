0018b4a2b2582496c841560932295fd32d950973
bilal-sagir <sagir.bilal@icloud.com>
2021-11-23 20:37:32 +0300

    save and fetch an image

--- PersistentData/ViewController.swift
+++ PersistentData/ViewController.swift
@@ -2,11 +2,34 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var myImgView : UIImageView!
 
-    override func viewDidLoad()
+    func imgUrl () -> URL
     {
-        super.viewDidLoad()
+        let docsDir = try? FileManager.default.url(for: .documentDirectory,
+                                                      in: .userDomainMask,
+                                                      appropriateFor: nil,
+                                                      create: false)
+        print(docsDir)
+        return docsDir!.appendingPathComponent("myImg.jpeg")
     }
-
+    
+    @IBAction func saveImg()
+    {
+        let imgData = myImgView.image?.jpegData(compressionQuality: 1.0)
+        do{
+            try? imgData?.write(to: imgUrl())
+            myImgView.image = nil
+        }
+    }
+    
+    @IBAction func fetchImg(){
+        if let data = try? Data(contentsOf: imgUrl())
+        {
+            let img = UIImage(data: data)
+            myImgView.image = img
+        }
+               
+    }
 }
 
